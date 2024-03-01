bb90a39ea29e7f32c2b17fac16361da597671512
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2024-03-01 14:35:21 +0700

    Latihan Ketiga

---
+++ tipe-data.py
@@ -0,0 +1,11 @@
+# Formatted String
+name = "M Fathir Arya Nafis"
+
+print(f"Hello, Nama Saya {name}")
+
+
+# %-formatting
+print("Nama Saya %s" % (name))
+
+# str.format()
+print("Nama Saya {}" .format(name))
